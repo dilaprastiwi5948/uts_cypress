a9078ba1c5b11365e75ae0433488ab86ba9e0e5b
addina nurul <addinanurulmawaddah39@gmail.com>
2021-11-24 14:35:47 +0700

    commit test cart _ ArhamIzza

---
+++ ProjectLogin/cypress/integration/TestCase5.js
@@ -0,0 +1,25 @@
+//Nama          : Arham Izza Syany
+//NIM/N0        : 1941720127 (03)
+//Kelas         : TI-3B
+//Test Case     : Checkout Tanpa menambah belanjaan 
+
+describe('Checkout Tanpa menambah belanjaan ', () => {
+    it('TestCase5', () => {
+    //masuk ke web saucedemo
+    cy.visit('https://www.saucedemo.com/')
+      //akses ke username
+      cy.get('#user-name')
+      //menulis username
+      .type('standard_user')
+       //akses ke password
+      cy.get('#password')
+      //menuliskan password
+      .type('secret_sauce')
+      //klik tombol login
+      cy.contains('Login').click()
+      //klik tombol cart
+      cy.get('.shopping_cart_link').click()
+      //klik Checkout
+      cy.get('#checkout').click()
+    })
+  })
